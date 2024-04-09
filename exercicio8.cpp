c17b4c517d96fa47f892fc9805d14e5c092ddcd5
kadu567 <162385570+kadu567@users.noreply.github.com>
2024-04-09 22:40:37 +0000

    main.cpp

---
+++ exercicio8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+    int num, num2;
+    printf("Insira sua altura em metros: ");
+    scanf("%d", &num);
+    printf("Insira seu peso em quilogramas: ");
+    scanf("%d", &num2);
+    
+    if (num2 / num * num < 18,5) {
+        printf("Categoria: Abaixo do peso");
+    } else if (num2 / num * num > 18,5 == 25){
+    printf("Categoria: Peso normal");
+    } else if (num2 / num * num > 30){
+        printf("")
+
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
